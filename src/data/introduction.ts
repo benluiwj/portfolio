fe726debfa9ed0f865964e76aaf97477b8308031
benluiwj <wenjie.lui@gmail.com>
2023-06-11 12:27:20 +0800

    chore: update color and content

--- src/data/introduction.ts
+++ src/data/introduction.ts
@@ -1,5 +1,5 @@
 export const introduction: string =
-  "Hi! My name is Benjamin Lui, and I enjoy using software to solve problems.";
+  "Hi! My name is Benjamin Lui, and I enjoy building software to solve problems."
 
 export const elaboration: string =
-  "I am currently a penultimate student in the National University of Singapore studying Computer Science. I have always been intrigued in finding different ways to use technology to solve problems that are interesting and challenging. Apart from staring at the screen, I enjoy watching movies, doing sports and photography too.";
+  "I am currently a final year student in the National University of Singapore studying Computer Science. I have always been intrigued in finding different ways to use technology to solve problems that are interesting and challenging. Apart from writing code, I enjoy watching movies, doing sports and photography too."
